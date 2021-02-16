046023ae70ed6b75d2392925baa4f6f5dd9df206
MrDavona <mrdavona@gmail.com>
2021-02-17 00:18:03 +0400

    Initial Commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    
+    private Vector3 _offset = new Vector3(0, 5, -10);
+    public GameObject player;
+    
+    void LateUpdate()
+    {
+        // Camera follows the player
+        transform.position = player.transform.position + _offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables
+    private Rigidbody _playerRb;
+    private float _horizontalInput;
+    private float _forwardInput;
+    
+    public float speed = 10.0f;
+
+    void Start()
+    {
+        // _playerRb = GetComponent<Rigidbody>();
+        // playerRb.centerOfMass = centerOfMass.transform.position;
+    }
+
+    void Update()
+    {
+        // This is where we get player input
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _forwardInput = Input.GetAxis("Vertical");
+
+        // Move the Player forward
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+
+        // Move the Player left and right depend on user input
+        if (_horizontalInput != 0)
+        {
+            transform.Translate(Vector3.right * (Time.deltaTime * speed * _horizontalInput));
+        }
+    }
+
+    bool isOnGround()
+    {
+        return true;
+    }
+
+}
