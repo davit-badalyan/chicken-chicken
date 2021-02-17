6657f74bac791d4859f6c6479ce5b480aeff2f4a
MrDavona <mrdavona@gmail.com>
2021-02-18 00:02:47 +0400

    Moving forward on keyboard input by changing legs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,32 +5,57 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Variables
-    private Rigidbody _playerRb;
     private float _horizontalInput;
     private float _forwardInput;
     
+    public GameObject leftLeg;
+    public GameObject rightLeg;
     public float speed = 10.0f;
 
     void Start()
     {
         // _playerRb = GetComponent<Rigidbody>();
         // playerRb.centerOfMass = centerOfMass.transform.position;
+        leftLeg.SetActive(false);
     }
 
     void Update()
     {
         // This is where we get player input
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _forwardInput = Input.GetAxis("Vertical");
+        // _horizontalInput = Input.GetAxis("Horizontal");
+        // _forwardInput = Input.GetAxis("Vertical");
 
         // Move the Player forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (leftLeg.activeSelf)
+            {
+                transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+                // transform.Rotate(Vector3.forward * (Time.deltaTime * 30));
+
+                leftLeg.SetActive(false);
+                rightLeg.SetActive(true);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (rightLeg.activeSelf)
+            {
+                transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+                // transform.Rotate(Vector3.back * (Time.deltaTime * 30));
+                
+                rightLeg.SetActive(false);
+                leftLeg.SetActive(true); 
+            }
+        }
+
+
 
         // Move the Player left and right depend on user input
-        if (_horizontalInput != 0)
-        {
-            transform.Translate(Vector3.right * (Time.deltaTime * speed * _horizontalInput));
-        }
+        // if (_horizontalInput != 0)
+        // {
+        //     transform.Translate(Vector3.right * (Time.deltaTime * speed * _horizontalInput));
+        // }
     }
 
     bool isOnGround()
